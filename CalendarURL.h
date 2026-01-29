4bfcaa1231f65c9c29add23a1a61de80bf9a7e90
Clemens Runge <cle.runge@pm.me>
2026-01-29 10:06:26 +0100

    Add secret file templates

---
+++ CalendarURL.h
@@ -0,0 +1,2 @@
+// https://www.awsh.de/service/abfuhrtermine/
+const char* icsURL = "https://www.awsh.de/api_v2/*************/2025.ics";
---
+++ WifiSecret.h
@@ -0,0 +1,2 @@
+const char* SSID = "my-ssid";
+const char* PSWD = "my-pw";
